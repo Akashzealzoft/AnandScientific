4ca0f794c54398f8c9df8de85988ab8184977acd
ariefzealzoft <arief@zealzoft.com>
2024-01-18 16:24:24 +0530

    some changes done in abouT

--- components/Blog/index.tsx
+++ components/Blog/index.tsx
@@ -12,8 +12,20 @@ const Blog = () => {
       <div className="container">
         <SectionTitle
           title="Our Latest Blogs"
-          paragraph="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
-          center
+
+          paragraph=""
+          paragraph1=""
+          teamTitle=""
+          teamDescription=""
+          sectionTeamList=""
+          deal=""
+          dealContent1=""
+          dealContent=""
+          infra=""
+          infraDescription=""
+          mb="44px"
+          center={true}
+          width=""
         />
 
         <div className="grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2 md:gap-x-6 lg:gap-x-8 xl:grid-cols-3">
